835498eef1512fccc8b998141db20e2e99362366
imane-CDA <boujnane.imaane@gmail.com>
2026-06-17 20:09:49 +0200

    Initial commit SmartPlanner

---
+++ SmartPlanner/SmartPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPlannerApp.swift
+//  SmartPlanner
+//
+//  Created by Apprenant131 on 17/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SmartPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
